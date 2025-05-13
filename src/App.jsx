53dea4f62fbb54369c4c8042202d1d5152b4e7a9
Sebastian Wilson <sebastian.j.wilson.2006@gmail.com>
2025-05-13 16:33:28 -0500

    Changes to components and html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import FileUpload from './Components/FileUpload'
 
 function App() {
   return (
     <>
-      <div>
+      <div id='BackgroundDIV'>
         <h1>Roster Generation</h1>
+        <FileUpload/>
       </div>
     </>
   )
---
+++ src/Components/FileUpload.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const FileUpload = () => {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            {selectedFile && (
+                <p>Selected file: {selectedFile.name}</p>
+            )}
+        </div>
+    )
+}
+
+export default FileUpload;
