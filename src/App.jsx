0abc6896dca9d73e8d03f754fe7530b4a23787eb
Sebastian Wilson <sebastian.j.wilson.2006@gmail.com>
2025-05-13 17:31:48 -0500

    Added trash button to fileComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import FileUpload from './Components/FileUpload'
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
   return (
     <>
       <h1>Roster Generation</h1>
-      <FileUpload />
+      <FileUpload selectedFile={selectedFile} setSelectedFile={setSelectedFile} />
+      <p>{selectedFile != null && 'Has File' || ''}</p>
     </>
   )
 }
--- src/Components/FileUpload.jsx
+++ src/Components/FileUpload.jsx
@@ -1,22 +1,29 @@
 import { useState, useRef } from "react";
 import { Button } from "@radix-ui/themes";
-import { FileIcon } from "@radix-ui/react-icons";
+import { FileIcon, TrashIcon } from "@radix-ui/react-icons";
 
-const FileUpload = () => {
-
-    const [selectedFile, setSelectedFile] = useState(null);
+const FileUpload = ({selectedFile, setSelectedFile}) => {
     const fileInputRef = useRef();
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
+    const deleteFile = () => {
+        setSelectedFile(null);
+    }
+
     return (
         <>
             <Button onClick={()=>fileInputRef.current.click()}>
                 <FileIcon/>
                 {selectedFile != null ? selectedFile.name : 'Browse CSV'}
-            </Button>
+            </Button>{' '}
+            {selectedFile && (
+                <Button color="red" onClick={deleteFile}>
+                    <TrashIcon style={{ transform: 'scale(1.25)' }}/>
+                </Button>
+            )}
             <input onChange={handleFileChange} multiple={false} ref={fileInputRef} accept=".csv" type='file'hidden/>
         </>
     )
